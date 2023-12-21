917f851f98903e816c42844d21acff94a480fa4b
Ragunath77 <153708386+Ragunath77@users.noreply.github.com>
2023-12-21 11:40:12 +0530

    Add files via upload
    Signed-off-by: Ragunath77 <153708386+Ragunath77@users.noreply.github.com>

---
+++ src/Components/assign.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './assign.css';
+import TemporaryDrawer from './Drawer';
+
+const MyAppBar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Button color="inherit" onClick={toggleDrawer} sx={{ marginLeft:-2 }}>
+          <TemporaryDrawer open={isDrawerOpen} onClose={toggleDrawer} />
+        </Button>
+        <Typography variant="h6" sx={{ flexGrow: 1 , marginLeft:-1 }}>
+          EMS
+        </Typography>
+        <Button color="inherit" component={Link} to="/home" sx={{ marginRight: 1 }}>
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/about" sx={{ marginRight: 1 }}>
+          About Us
+        </Button>
+        <Button color="inherit" component={Link} to="/contact" sx={{ marginRight: 1 }}>
+          Contact Us
+        </Button>
+        <Button color="inherit" component={Link} to="/faq" sx={{ marginRight: 1 }}>
+          FAQs
+        </Button>
+        <Button color="inherit" component={Link} to="/">
+          Log out
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MyAppBar;
---
+++ src/Components/home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './assign.css';
+import emp from './emp.png';
+import emp1 from './emp1.png';
+import MyAppBar from './assign';
+import Footer from './footer';
+const HomePage = () => {
+  return (
+    <div>
+    <MyAppBar/>
+    <div className="Boxx">
+    <div className="i1"><img src={emp1} alt=''/></div>
+    <div className="i2"><img src={emp} alt=''/></div>
+    <div>
+      <center><h1>WELCOME TO<br/>THE<br/>HOME PAGE!</h1></center>
+    </div>
+    </div>
+       <div className="cl">
+       <Footer/>
+       </div>
+    </div>
+  );
+};
+
+export default HomePage;
